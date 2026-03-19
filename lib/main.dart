3212bd04516f4be1938dbff64341d55707b68c57
Gem <mojeedibrahim13@gmail.com>
2026-03-19 07:58:46 -0700

    appBar and body

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "App Bar"
+          ),
+          backgroundColor: Colors.deepPurpleAccent,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(
+              onPressed: () {}, 
+              icon: Icon(Icons.logout),
+            )
+          ],
+        ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+          //1st container
+            Container(
+              width: 250,
+              height: 200,
+              color: const Color.fromARGB(255, 126, 32, 3),
+            ),
+
+            //2nd container
+            Container(
+              width: 230,
+              height: 200,
+              color: const Color.fromARGB(255, 67, 126, 3),
+            ),
+
+            //3rd container
+            Container(
+              width: 200,
+              height: 130,
+              color: const Color.fromARGB(255, 3, 182, 107),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
